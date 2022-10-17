b57b7f44377cc59e6b3cc1ab362209e2fdb5f141
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-17 05:45:38 -0500

    styles topnav

--- components/layouts/TopNav.js
+++ components/layouts/TopNav.js
@@ -1,11 +1,31 @@
 import Link from "next/link"
+import Image from 'next/image'
+import Logo from '../../public/parivest1.png'
+import userPhoto from '../../public/user.jpeg'
+import styles from './Layout.module.css';
+import { TfiReload } from 'react-icons/tfi'
+import { IoIosNotificationsOutline } from 'react-icons/io'
 
-function TopNav () {
+function TopNav (props) {
   return (
-    <nav>
-      <ul>
-        <li>Parivest</li>
-        <li><Link href="/">Home</Link></li>
+    <nav className={styles.topnav}>
+      <div className={styles.navImage}>
+        <Image src={Logo} alt="logo" layout='responsive' />
+      </div>
+      <ul className={styles.lists}>
+        <li><Link href="/"><TfiReload /></Link></li>
+        <li><Link href="/"><IoIosNotificationsOutline /></Link></li>
+        <li>
+          <div className={styles.user}>
+            <div>
+              <span>Sun Kim</span> <br />
+              <small>Super Admin</small>
+            </div>
+            <div className={styles.userImage}>
+              <Image src={userPhoto} alt="user" layout='responsive' />
+            </div>
+          </div>
+        </li>
       </ul>
     </nav>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-// import Image from 'next/image'
 import Dashboard from '../components/Dashboard'
 import { Fragment } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <Fragment className={styles.container}>
+    <Fragment>
       <Head>
         <title>Parivest | Dashboard</title>
         <meta name="description" content="Parivest users Dashboard" />
