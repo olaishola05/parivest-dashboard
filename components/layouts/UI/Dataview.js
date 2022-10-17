e67d48e7fb45d71bc16b8ab5cd04e9a4342996e0
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-17 09:02:31 -0500

    Add the select and other tags to top of dataview

--- components/layouts/UI/Dataview.js
+++ components/layouts/UI/Dataview.js
@@ -1,5 +1,30 @@
+import styles from './Dashboard.module.css'
+
 function Dataview(props){
-  return <h1>This is the view</h1>
+  return (
+    <div className={styles.dataview}>
+      <div className={styles.datatop}>
+        <div className={styles.customselect}>
+          <select name="dataview" id="dataview">
+            <option className={styles.options} value="all">All</option>
+            <option value="approved">Approved</option>
+            <option value="pending">Pending</option>
+            <option value="review">In-review</option>
+          </select>
+        </div>
+
+        <div className={styles.search}>
+          <div className={styles.date}>
+            <label htmlFor="date">From </label>
+            <input type="date" name="from" id="from" />
+            <label htmlFor="date">To </label>
+            <input type="date" name="to" id="to" />
+          </div>
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dataview
