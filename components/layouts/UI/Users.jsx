75f7d5758fe46aef11a94406f4ece052da8a5e4c
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-19 14:17:34 -0500

    Create new component Users

---
+++ components/layouts/UI/Users.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import Dashboard from './Dashboard'
+import UserTop from './UserTop'
+import UserTable from './UserTable'
+
+function Users(props) {
+  const [data, setData] = useState({ data: [] })
+
+  useEffect(() => {
+    const dataObj = props?.users?.data;
+    setData((prev) => ({ ...prev, data: [...dataObj] }));
+  }, [props])
+
+  const userData = data?.data?.map((i, index) => {
+    return { ...i, key: index };
+  });
+  return (
+    <Dashboard>
+      <UserTop />
+      <UserTable userData={userData} />
+    </Dashboard>
+  )
+}
+
+export default Users
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import Dashboard from '../../components/layouts/UI/Dashboard'
-import axios from 'axios'
-import UserTop from '../../components/layouts/UI/UserTop'
-import UserTable from '../../components/layouts/UI/UserTable'
+import axios from "axios";
+import Users from "../../components/layouts/UI/Users";
 
-function users(props) {
-  // const [data, setData] = useState({ data: [] })
-
-  // useEffect(() => {
-  //   const dataObj = props?.dataObj?.data[0]?.data;
-  //   setData((prev) => ({ ...prev, data: [...dataObj] }));
-  // }, [props])
-
-  const userData = props?.dataObj?.data[0]?.data?.map((i, index) => {
-    return { ...i, key: index };
-  });
+function UserContainer(props){
+  const { dataObj } = props
   return (
-    <Dashboard>
-      <UserTop />
-      <UserTable userData={userData}/>
-    </Dashboard>
+    <Users users={dataObj} />
   )
 }
 
-export default users
+export default UserContainer;
 
 
 export async function getServerSideProps(context) {
