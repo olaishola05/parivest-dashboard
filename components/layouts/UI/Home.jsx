286954337af0c92b73d1dfb11842ac2371a43a2e
Ola Ishola <olaishola@hotmail.co.uk>
2022-10-24 13:40:00 -0500

    Add components to home pages

--- components/layouts/UI/Home.jsx
+++ components/layouts/UI/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Dashboard from './Dashboard'
+import { Typography, Card, Col, Row } from 'antd';
+
+const { Title } = Typography;
 
 function Home() {
   return (
     <div>
       <Dashboard>
-        <h1>Work in progress</h1>
+        <Title>Welcome to Admin Dashboard</Title>
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card title="Card title" bordered={false}>
+                Card content
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="Card title" bordered={false}>
+                Card content
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="Card title" bordered={false}>
+                Card content
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </Dashboard>
     </div>
   )
